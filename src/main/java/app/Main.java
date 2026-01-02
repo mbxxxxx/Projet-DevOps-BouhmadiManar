a8482a8159dba5e2127e3d45815422bd3526ccc5
Alice Torvalds <alice@linux.org>
2026-01-02 13:14:02 +0100

    Update message in DEV

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,6 +2,6 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("mon projet en DevOps");
+        System.out.println("Bonjour DEV - Projet DevOps");
     }
 }
